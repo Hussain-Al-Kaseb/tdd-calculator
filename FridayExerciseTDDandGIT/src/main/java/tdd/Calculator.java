64ea54b463757354c0e5605560d20362a98148b8
Hussain-Al-Kaseb <hussainfhl@hotmail.com>
2025-08-22 16:13:58 +0200

    git status
    Psda
    git pull origin main --allow-unrelated-histories
    git pull origin main --allow-unrelated-histories
    ms
    
    lease enter the commit message for your changes. Lines starting

---
+++ FridayExerciseTDDandGIT/src/main/java/tdd/Calculator.java
@@ -0,0 +1,19 @@
+package tdd;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+    public int calcAdd(int a, int b) {
+        return a + b;
+    }
+    public int calcSub(int a, int b) {
+        return a - b;
+    }
+    public int calcMultiply(int a, int b) {
+        return a * b;
+    }
+    public int calcDivide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ FridayExerciseTDDandGIT/src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,47 @@
+package tdd;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    Calculator calc = new Calculator();
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+    @Test
+    void add() {
+        int actual = calc.calcAdd(2,2);
+        int expected = 4;
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void sub() {
+        int actual = calc.calcSub(2,2);
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void multiply() {
+        int actual = calc.calcMultiply(2,2);
+        int expected = 4;
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void divide() {
+        int actual = calc.calcDivide(2,2);
+        int expected = 1;
+        assertEquals(expected, actual);
+
+    }
+
+
+}
